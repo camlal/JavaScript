a826c1e33298bfcaee29b118158bdec22ee28e4e
Miguel <catarina.alvesmelo16@gmail.com>
2024-06-18 20:30:57 -0300

    JavaScript

--- aula14/ex14.js
+++ aula14/ex14.js
@@ -1,15 +1,17 @@
 const hamburgerButton = document.querySelector("#hamburgerButton");
-const closeButton = document.querySelector("#closeButton");
+//estamos recuperando o botãoa de hamburguer (fora da div)  
 
+const closeButton = document.querySelector("#closeButton");
+//estamos recuperando o botão de fechamento 
 
 const mobileMenu = document.querySelector("#mobileMenu");
+//estamos recuperando a div (quando aberta no menu de hamburguer)
+
 
 
 hamburgerButton.addEventListener("click", function() {
     mobileMenu.classList.add("flex");
 });
-
-
 
 closeButton.addEventListener("click", function() {
     mobileMenu.classList.remove("flex");
